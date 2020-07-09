9abbf9fbce30b6c78f1b96df50b8c435193b714c
carlosxjose <carlosxjose@gmail.com>
2020-07-08 22:25:27 -0400

    StackLayout and Grid versions...

---
+++ CursoXamarin/Tareas/Tarea12.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoXamarin.Tareas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tarea12 : ContentPage
+    {
+        public Tarea12()
+        {
+            InitializeComponent();
+        }
+    }
+}
